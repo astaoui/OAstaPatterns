a72b0abd78961caf861a54a796ccd4001ccabd33
astaoui <med.astaoui@gmail.com>
2016-11-03 23:50:07 +0000

    Initial commit

---
+++ src/factoryPattern/Produit2.java
@@ -0,0 +1,12 @@
+package factoryPattern;
+
+/**
+ * Created by medasta on 03/11/2016.
+ */
+class Produit2 extends Produit{
+
+    @Override
+    public void show() {
+        System.out.println("Product 2 was called");
+    }
+}
