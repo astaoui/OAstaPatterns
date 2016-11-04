155b630978b4a51c4d474ea1450d906e340cc1ac
astaoui <med.astaoui@gmail.com>
2016-11-04 00:20:43 +0000

    Adding The Builder Pattern

---
+++ src/builderPattern/ChickenBurger.java
@@ -0,0 +1,16 @@
+package builderPattern;
+
+/**
+ * Created by medasta on 04/11/2016.
+ */
+public class ChickenBurger extends Burger {
+    @Override
+    public String name() {
+        return "Chicken Burger";
+    }
+
+    @Override
+    public float price() {
+        return 50.5f;
+    }
+}
---
+++ src/builderPattern/Packing.java
@@ -0,0 +1,8 @@
+package builderPattern;
+
+/**
+ * Created by medasta on 03/11/2016.
+ */
+public interface Packing {
+    public String pack();
+}
